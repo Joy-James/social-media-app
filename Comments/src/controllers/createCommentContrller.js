86dbe00ac7bab71abda154b2443becf330d3e706
Mumbi njuguna <mumbijoynjuguna@gmail.com>
2023-07-06 23:36:26 +0300

    creating a comment and updating the same comment

---
+++ Comments/src/controllers/createCommentContrller.js
@@ -0,0 +1,65 @@
+const mssql = require('mssql');
+const {config} = require('../config/config');
+
+
+
+async function createComment  (req, res)  {
+    try {
+      const { post_id, UserprofileID, comment } = req.body;
+      const sql = await mssql.connect(config);
+      if (sql.connected) {
+        const request = sql.request()
+          .input("post_id", post_id)
+          .input("UserprofileID", UserprofileID)
+          .input("comment", comment)
+          .execute("dbo.CreateComment");
+  
+        await request;
+  
+        res.json({
+          success: true,
+          message: "Comment created successfully"
+        });
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+
+  async function updateComment (req, res)  {
+    try {
+      const { post_id, comment_id, comment } = req.body;
+      const sql = await mssql.connect(config);
+      if (sql.connected) {
+        const request = sql.request()
+          .input("post_id", post_id)
+          .input("comment_id", comment_id)
+          .input("comment", comment)
+          .execute("dbo.UpdateComment");
+  
+        const result = await request;
+        const rowsAffected = result.rowsAffected[0];
+  
+        if (rowsAffected === 0) {
+          // No rows affected, comment with the given post ID and comment ID doesn't exist
+          res.status(404).json({
+            success: false,
+            message: "Comment not found"
+          });
+        } else {
+          // Comment updated successfully
+          res.json({
+            success: true,
+            message: "Comment updated successfully"
+          });
+        }
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  }
+  
+  
+  module.exports={createComment, updateComment}
