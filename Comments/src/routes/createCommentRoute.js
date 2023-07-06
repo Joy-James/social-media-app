c92e6b430ed880d4210c1e8294da1dc1a9cbee44
Mumbi njuguna <mumbijoynjuguna@gmail.com>
2023-07-07 00:01:11 +0300

    delete a comment made by user

--- Comments/src/routes/createCommentRoute.js
+++ Comments/src/routes/createCommentRoute.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const createRouter=express.Router()
-const{createComment,updateComment} = require('../controllers/createCommentContrller')
+const{createComment,updateComment,deleteComment} = require('../controllers/createCommentContrller')
 
 createRouter.post('/posts/:comment',  createComment )
 createRouter.put('/posts/:update',  updateComment )
+createRouter.delete('/posts/:delete',  deleteComment )
 
 
 module.exports=  createRouter;
