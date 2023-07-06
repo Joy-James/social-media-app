a41eb0a7dfc59a75404f051bc650174957fa6304
Mumbi njuguna <mumbijoynjuguna@gmail.com>
2023-07-06 12:59:48 +0300

    nodemailer in the signup of a new user

--- Authentication/server.js
+++ Authentication/server.js
@@ -1,26 +1,27 @@
-const express =require('express')
+const express = require('express')
+
 require("dotenv").config();
-const app=express();
-const session=require('express-session')
-const { v4 }=require('uuid')
-const router=require('./src/routes/usersRoutes');
-const userRouter=require('./src/routes/regestrationRoute');
-const { user} = require('./src/config/config');
+const app = express();
+const session = require('express-session')
+const { v4 } = require('uuid')
+const router = require('./src/routes/usersRoutes');
+const userRouter = require('./src/routes/regestrationRoute');
+const { user } = require('./src/config/config');
 app.use(express.json())
 
-const oneDay=60*60*1000*24
+const oneDay = 60 * 60 * 1000 * 24
 
 app.use(session({
-    secret:process.env.SECRET,
-    saveUninitialized:true,
-    genid:()=>v4(),
-    resave:true,
-    rolling:true,
-    cookie:{
-     httpOnly:true,
-     secure:true,
-     maxAge:oneDay,
-     secure:false
+    secret: process.env.SECRET,
+    saveUninitialized: true,
+    genid: () => v4(),
+    resave: true,
+    rolling: true,
+    cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge: oneDay,
+        secure: false
     }
 }))
 // app.get("/login/:UserProfileID/:pass", (req,res) =>{
@@ -35,10 +36,12 @@ app.use(session({
 app.use('/', router)
 app.use('/post', userRouter)
 console.log(v4())
-app.get('/logout', (req, res)=>{
- req.session.destroy();
- res.send('logout successfully')
+
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.send('logout successfully')
 })
 
 const port = process.env.PORT;
-app.listen(port,()=>console.log(`server on port:${port}`))
+app.listen(port, () => console.log(`server on port:${port}`))
---
+++ Authentication/src/utils/sendMail.js
@@ -0,0 +1,30 @@
+const { createTransport } = require("nodemailer");
+require("dotenv").config();
+
+const {email_config} = require('../config/emailConfig');
+const transporter = createTransport(email_config);
+
+async function sendMail( _, full_name) {
+  const message_options = {
+    from: process.env.EMAIL_USER,
+    to: ["joyisvoiced@gmail.com"],
+    subject: "Welcome to our BUDDEE Community!",
+    text: `Dear ${full_name},
+    
+    We are delighted to welcome you to our BUDDEE community! On behalf of our entire team.
+    Should you have any questions or require assistance, please don't hesitate to reach out to our friendly staff.
+    We are here to make your experience with us enjoyable and fulfilling.
+    Once again, welcome to our BUDDEE family! 
+    Happy connecting!`
+  };
+
+  try {
+    console.log(email_config)
+    const info = await transporter.sendMail(message_options);
+    console.log("Message sent: %s", info.messageId);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = { sendMail };
